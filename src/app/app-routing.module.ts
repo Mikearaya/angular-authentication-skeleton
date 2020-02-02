dafbe9227575665f73b98aebbaf9d4baaf1e41b5
mikearaya <mikaelaraya12@gmail.com>
2020-02-02 14:31:45 +0300

    updated route unauthorized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { UnauthorizedUserComponent } from "./unauthorized-user/unauthorized-user.component";
 
 const routes: Routes = [
   {
@@ -20,6 +21,10 @@ const routes: Routes = [
       import("./user-section/user-section.module").then(
         m => m.UserSectionModule
       )
+  },
+  {
+    path: "unauthorized-user",
+    component: UnauthorizedUserComponent
   }
 ];
 
